8f5ea1a6b5c8fb214f73deaa080a404178443273
ZaikaDaria <corplinebusiness@gmail.com>
2023-08-12 00:05:50 +0300

    Changed the return for movie_image_file_path in airport/models.py

--- airport/models.py
+++ airport/models.py
@@ -54,7 +54,7 @@ def movie_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/crew/", filename)
+    return os.path.join("uploads", "crew", filename)
 
 
 class Route(models.Model):
